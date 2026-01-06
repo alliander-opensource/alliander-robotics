feeb5275ddbd6e76609edcdfa76f9b2cdf915634
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2026-01-06 15:58:08 +0000

    Improve Dev container.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    Signed-off-by: Peter Geurts <peter.geurts@proton.me>

--- common/colcon_build.sh
+++ common/colcon_build.sh
@@ -8,7 +8,7 @@ cd /rcdt/ros
 uv sync
 . /opt/ros/$ROS_DISTRO/setup.sh
 colcon build --symlink-install \
-  --cmake-args -DCMAKE_BUILD_TYPE=Release 
+  --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> /root/.bashrc
 echo "source /rcdt/ros/install/setup.bash" >> /root/.bashrc
