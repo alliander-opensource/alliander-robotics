bb3cd246225c86c07fe205ee2dfe9c51f88baf6a
Peter Geurts <peter.geurts@proton.me>
2026-01-26 11:46:50 +0100

    fixed merge errors
    Signed-off-by: Peter Geurts <peter.geurts@proton.me>

--- rcdt_gps/src/rcdt_gps/launch/hardware.launch.py
+++ rcdt_gps/src/rcdt_gps/launch/hardware.launch.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import itertools
-
 from launch import LaunchContext, LaunchDescription
 from launch.actions import OpaqueFunction
 from launch_ros.actions import Node
@@ -12,11 +10,6 @@ from rcdt_utilities.launch_argument import LaunchArgument
 from rcdt_utilities.register import Register
 
 platform_arg = LaunchArgument("platform_config", "")
-
-T = True
-F = False
-
-# Parameters based on https://github.com/ros-navigation/navigation2_tutorials/blob/rolling/nav2_gps_waypoint_follower_demo/config/dual_ekf_navsat_params.yaml
 
 
 def launch_setup(context: LaunchContext) -> list:
@@ -48,72 +41,6 @@ def launch_setup(context: LaunchContext) -> list:
             ("heading", "~/heading"),
             ("time_reference", "~/time_reference"),
             ("vel", "~/vel"),
-        ],
-    )
-
-    navsat_transform_node = Node(
-        package="robot_localization",
-        executable="navsat_transform_node",
-        namespace=namespace_gps,
-        parameters=[
-            {
-                "publish_filtered_gps": True,
-            }
-        ],
-        remappings=[("imu", f"/{namespace_vehicle}/imu/data")],
-    )
-
-    # Define EKF node that creates the tf between odom and map:
-    ekf_global = Node(
-        package="robot_localization",
-        executable="ekf_node",
-        name="ekf_global",
-        namespace=namespace_gps,
-        parameters=[
-            {
-                "two_d_mode": True,
-                "publish_tf": True,
-                "world_frame": "map",
-                "map_frame": "map",
-                "odom_frame": f"{namespace_vehicle}/odom",
-                "base_link_frame": f"{namespace_vehicle}/base_footprint",
-                "odom0": f"/{namespace_vehicle}/odometry/wheels",
-                "odom0_config": list(
-                    itertools.chain.from_iterable(
-                        [
-                            [F, F, F],  # [x_pos, y_pos, z_pos]
-                            [F, F, F],  # [roll, pitch, yaw]
-                            [T, T, T],  # [x_vel, y_vel, z_vel]
-                            [F, F, T],  # [roll_rate, pitch_rate, yaw_rate]
-                            [F, F, F],  # [x_accel, y_accel, z_accel]
-                        ]
-                    )
-                ),
-                "odom1": f"/{namespace_gps}/odometry/gps",
-                "odom1_config": list(
-                    itertools.chain.from_iterable(
-                        [
-                            [T, T, F],  # [x_pos, y_pos, z_pos]
-                            [F, F, F],  # [roll, pitch, yaw]
-                            [F, F, F],  # [x_vel, y_vel, z_vel]
-                            [F, F, F],  # [roll_rate, pitch_rate, yaw_rate]
-                            [F, F, F],  # [x_accel, y_accel, z_accel]
-                        ]
-                    )
-                ),
-                "imu0": f"/{namespace_vehicle}/imu/data",
-                "imu0_config": list(
-                    itertools.chain.from_iterable(
-                        [
-                            [F, F, F],  # [x_pos, y_pos, z_pos]
-                            [F, F, T],  # [roll, pitch, yaw]
-                            [F, F, F],  # [x_vel, y_vel, z_vel]
-                            [F, F, F],  # [roll_rate, pitch_rate, yaw_rate]
-                            [F, F, F],  # [x_accel, y_accel, z_accel]
-                        ]
-                    )
-                ),
-            }
         ],
     )
 
