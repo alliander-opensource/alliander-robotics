74028a87d02fb46b7047a740f589e8e458e9237b
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2026-03-16 16:35:11 +0000

    Fix linting.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- start.py
+++ start.py
@@ -125,11 +125,11 @@ class Compose:
             platform (Platform | None): platform to get config for, or None if not a platform.
             arguments (str): additional arguments for pytest.
 
-        Raises:
-            ValueError: if platform is not provided while a platform is needed.
-
         Returns:
             tuple[str, str, dict]: tuple consisting of the package, the config for compose, and additional config.
+
+        Raises:
+            ValueError: if platform is not provided while a platform is needed.
         """
         needs_platform = service_type in {"platform", "moveit", "nav2"}
         if needs_platform and platform is None:
