d7af84a89830439e1bb6bc849b326f30aaff4ede
Rosalie <rosalie.van.ark@alliander.com>
2026-03-31 09:59:33 +0200

    Add more logs
    Signed-off-by: Rosalie <rosalie.van.ark@alliander.com>
    Signed-off-by: Peter Geurts <peter.geurts@proton.me>

--- alliander_tests/src/alliander_tests/tests/test_nav2_navigation_lidar.py
+++ alliander_tests/src/alliander_tests/tests/test_nav2_navigation_lidar.py
@@ -3,15 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import contextlib
+import math
 import sys
 import time
 
-import pytest
 import rclpy
 from alliander_utilities.config_objects import Lidar, Vehicle, link
-from geometry_msgs.msg import PoseStamped, TransformStamped
+from geometry_msgs.msg import PoseStamped, TransformStamped, TwistStamped
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
 from rclpy.time import Time
+from sensor_msgs.msg import JointState
 from tf2_ros import TransformException  # ty: ignore[unresolved-import]
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
@@ -43,23 +45,46 @@ class _TestNavigationLidar:
         Raises:
             TimeoutError: When a timeout occurs.
         """
-        # 0) Wait for valid TF
+
+        # 0) Log callbacks
+        def joint_state_callback(msg: JointState) -> None:
+            joint_state: JointState = msg
+            if math.isnan(joint_state.position[0]):
+                test_node.get_logger().error("ERROR: joint state is NaN!")
+
+        test_node.create_subscription(
+            JointState,
+            f"/{self.platforms['vehicle'].namespace}/joint_states",
+            joint_state_callback,
+            10,
+        )
+
+        def odom_callback(msg: Odometry) -> None:
+            odom: Odometry = msg
+            if math.isnan(odom.pose.pose.position.x):
+                test_node.get_logger().error("ERROR: odom is NaN!")
+
+        test_node.create_subscription(
+            Odometry,
+            f"/{self.platforms['vehicle'].namespace}/odom",
+            odom_callback,
+            10,
+        )
+
+        def twist_callback(msg: TwistStamped) -> None:
+            twist: TwistStamped = msg
+            if math.isnan(twist.twist.linear.x):
+                test_node.get_logger().error("ERROR: cmd_vel is NaN!")
+
+        test_node.create_subscription(
+            TwistStamped,
+            f"/{self.platforms['vehicle'].namespace}/cmd_vel",
+            twist_callback,
+            10,
+        )
+        # 1) Obtain current pose in map frame:
         tf_buffer = Buffer()
         TransformListener(tf_buffer, test_node)
-        start = time.time()
-        while True:
-            rclpy.spin_once(test_node, timeout_sec=0.1)
-            try:
-                tf_buffer.lookup_transform("odom", f"{self.platforms['vehicle'].namespace}/base_footprint", Time())
-                break
-            except TransformException:
-                pass
-            if time.time() - start > timeout:
-                pytest.fail("panther/odom never appeared: odometry stack did not start")
-
-        # 1) Obtain current pose in map frame:
-        # tf_buffer = Buffer()
-        # TransformListener(tf_buffer, test_node)
         current_pose = TransformStamped()
 
         start_time = time.time()
@@ -97,6 +122,7 @@ class _TestNavigationLidar:
         start_time = time.time()
         distance: float = sys.float_info.max
         timed_out = False
+        last_log_time = 0.0
 
         while distance > navigation_distance_tolerance:
             rclpy.spin_once(test_node, timeout_sec=0)
@@ -107,9 +133,15 @@ class _TestNavigationLidar:
                 distance = abs(
                     current_pose.transform.translation.x - goal_pose.pose.position.x
                 )
+            now = time.time()
+            if now - last_log_time >= 1.0:
+                test_node.get_logger().info(f"Distance to goal: {distance:.2f}m")
+                last_log_time = now
             if time.time() - start_time > timeout:
                 timed_out = True
                 break
+
+        test_node.get_logger().info(f"Final TEST distance to goal: {distance}.")
 
         assert not timed_out, (
             f"Timeout: distance {distance} > tolerance {navigation_distance_tolerance}"
