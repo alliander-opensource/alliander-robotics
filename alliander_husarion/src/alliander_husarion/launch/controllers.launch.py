2c5da818343bcdbe8bc004fcc10b7f1f0814afae
Rosalie <rosalie.van.ark@alliander.com>
2026-04-08 12:51:30 +0200

    Check again whether no publishers to cmd_vel (and mismatch between husarion and nav2 package) causes no nans
    Signed-off-by: Rosalie <rosalie.van.ark@alliander.com>
    Signed-off-by: Peter Geurts <peter.geurts@proton.me>

--- alliander_husarion/src/alliander_husarion/launch/controllers.launch.py
+++ alliander_husarion/src/alliander_husarion/launch/controllers.launch.py
@@ -89,7 +89,7 @@ def launch_setup(context: LaunchContext) -> list:
             "--controller-manager",
             "controller_manager",
             "--controller-ros-args",
-            "--remap drive_controller/cmd_vel:=cmd_vel_nav",
+            "--remap drive_controller/cmd_vel:=cmd_vel_nav2",
             "--controller-ros-args",
             "--remap drive_controller/odom:=odometry/wheels",
             "--controller-ros-args",
@@ -100,7 +100,7 @@ def launch_setup(context: LaunchContext) -> list:
     )
 
     return [
-        Register.on_start(twist_mux, context),
+        # Register.on_start(twist_mux, context),
         Register.on_exit(joint_state_broadcaster_spawner, context),
         Register.on_exit(imu_broadcaster_spawner, context),
         Register.on_exit(drive_controller_spawner, context),
