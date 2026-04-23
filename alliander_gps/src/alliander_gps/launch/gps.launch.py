1f8d66d6d59414e5eeb167d3b87a43e6006237e0
Peter Geurts <peter.geurts@proton.me>
2026-04-23 14:25:57 +0200

    moved ekf/navsat nodes to nav2 as they now need imu config
    Signed-off-by: Peter Geurts <peter.geurts@proton.me>

--- alliander_gps/src/alliander_gps/launch/gps.launch.py
+++ alliander_gps/src/alliander_gps/launch/gps.launch.py
@@ -50,57 +50,11 @@ def launch_setup(context: LaunchContext) -> list:
         {"platform_config": gps_config.to_str()},
     )
 
-    ekf_global_params = AdaptedYaml(
-        get_file_path("alliander_gps", ["config"], "ekf_global.yaml"),
-        {
-            "odom_frame": f"{gps_config.parent.namespace}/odom",
-            "base_link_frame": f"{gps_config.parent.namespace}/base_footprint",
-            "odom0": f"/{gps_config.parent.namespace}/odometry/wheels",
-            "odom1": f"/{gps_config.namespace}/odometry/gps",
-            "imu0": f"/{gps_config.parent.namespace}/imu/data",
-        },
-        root_key=gps_config.parent.namespace,
-    )
-    navsat_transform_params = AdaptedYaml(
-        get_file_path("alliander_gps", ["config"], "navsat_transform.yaml"),
-        {},
-        root_key=gps_config.parent.namespace,
-    )
-
-    navsat_transform = Node(
-        package="robot_localization",
-        executable="navsat_transform_node",
-        name="navsat_transform",
-        namespace=gps_config.namespace,
-        parameters=[navsat_transform_params.file],
-        remappings=[
-            ("imu", f"/{gps_config.parent.namespace}/imu/data"),
-            (
-                "odometry/filtered",
-                f"/{gps_config.parent.namespace}/odometry/global",
-            ),
-        ],
-    )
-
-    # Define EKF node that creates the tf between odom and map:
-    ekf_global = Node(
-        package="robot_localization",
-        executable="ekf_node",
-        name="ekf_global",
-        namespace=gps_config.parent.namespace,
-        parameters=[ekf_global_params.file],
-        remappings=[
-            ("odometry/filtered", f"/{gps_config.parent.namespace}/odometry/global"),
-        ],
-    )
-
     return [
         SetParameter(name="use_sim_time", value=gps_config.simulation),
         Register.on_start(state_publisher, context),
         Register.on_start(static_tf, context),
         Register.group(hardware, context) if not gps_config.simulation else SKIP,
-        Register.on_start(navsat_transform, context) if parent.link else SKIP,
-        Register.on_start(ekf_global, context) if parent.link else SKIP,
     ]
 
 
